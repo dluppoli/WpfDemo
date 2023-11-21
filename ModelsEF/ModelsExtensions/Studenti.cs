abfbc7a8db76e31677320b453d9a1398b70ad2c7
Davide Luppoli <davide@davideluppoli.it>
2023-11-21 10:15:23 +0100

    Model Validation + Cnverters

---
+++ ModelsEF/ModelsExtensions/Studenti.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDemo.ModelsEF
+{
+    public partial class Studenti : IDataErrorInfo
+    {
+        public string this[string columnName]
+        {
+            get {
+                if(columnName=="Cognome")
+                {
+                    if (string.IsNullOrEmpty(Cognome))
+                        return "Obbligatorio";
+                    else
+                        return null;
+                }
+                if (columnName == "Nome")
+                {
+                    return string.IsNullOrEmpty(Nome) ? "Obbligatorio" : null;
+                }
+                return null;
+            } 
+        }
+
+
+        public string Error => null;
+
+
+        public bool InRegolaConPagamenti { get; set; } = false;
+    }
+}
